62251f2e3634801e2f20ea74b000db3e2cf40f93
王成阔 <wangchengkuo@rongcloud.cn>
2017-04-12 16:14:12 +0800

    feat:公众号

---
+++ web-sdk-test/integrate-demo/js/public-info.js
@@ -0,0 +1,41 @@
+/**
+ * Created by wangchengkuo on 17/4/12.
+ */
+function getPublicInfo(_options) {
+    var options = {
+        props:['stat'],
+        template: 'public/public-info.html',
+        methods: {
+            enterPublic:function () {
+                this.stat.currentView='publicChat';
+            },
+            unsubscribePublic:function () {
+                var publicServiceType = RongIMLib.ConversationType.APP_PUBLIC_SERVICE; //固定值
+                var publicServiceId=this.stat.currentPublic.publicServiceId;
+                RongIMClient.getInstance().unsubscribePublicService(publicServiceType, publicServiceId, {
+                    onSuccess:function(list){
+                        console.log("取消订阅公众号 成功");
+                        console.log(list);
+                    },
+                    onError:function(error){
+                        console.log("取消订阅公众号 失败");
+                    }
+                });
+            },
+            subscribePublic:function(){
+                var publicServiceType = RongIMLib.ConversationType.APP_PUBLIC_SERVICE; //固定值
+                var publicServiceId=this.stat.currentPublic.publicServiceId;
+                RongIMClient.getInstance().unsubscribePublicService(publicServiceType, publicServiceId, {
+                    onSuccess:function(list){
+                        console.log("订阅公众号 成功");
+                        console.log(list);
+                    },
+                    onError:function(error){
+                        console.log("订阅公众号 失败");
+                    }
+                });
+            }
+        }
+    };
+    return common.getComponent(options);
+}
---
+++ web-sdk-test/integrate-demo/js/public-list.js
@@ -0,0 +1,33 @@
+/**
+ * Created by wangchengkuo on 17/4/12.
+ */
+function getPublicList(_options) {
+    var options = {
+        props:['stat'],
+        template: 'public/public-list.html',
+        methods: {
+            getPublicServiceList: function () {
+                /*
+                 getRemotePublicServiceList = function (mpId, conversationType, pullMessageTime, callback)
+                 */
+                var that=this;
+                RongIMClient.getInstance().getPublicServiceList({
+                    onSuccess: function (list) {
+                        console.log("获取已关注公众号 成功");
+                        that.stat.publicList=list;
+                    },
+                    onError: function (error) {
+                        console.log("获取已关注公众号 失败");
+                    }
+                });
+            },
+            publicAdd:function(){
+                this.stat.currentView="publicSearch";
+            }
+        },
+        mounted:function () {
+            this.getPublicServiceList();
+        }
+    };
+    return common.getComponent(options);
+}
