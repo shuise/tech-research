004965455dd3da9c1c6c78bc0382c143b90adc75
王成阔 <wangchengkuo@rongcloud.cn>
2017-04-14 10:47:46 +0800

    fix:列表页取消上一步 搜索页面取消公众号标题 搜索结果只包含未关注 查看历史按钮 聊天页面多图文UI

--- web-sdk-test/integrate-demo/js/public-search.js
+++ web-sdk-test/integrate-demo/js/public-search.js
@@ -3,47 +3,53 @@
  */
 function getPublicSearch(_options) {
     var options = {
-        props:['stat'],
+        props: ['stat'],
         template: 'public/public-search.html',
         methods: {
-            publicSearch:function () {
+            publicSearch: function () {
 
-                var that=this;
-                $.getJSON('mockData.json',function (data) {
-                    console.log(data);
-                    that.stat.searchList=data.searchList;
-                    return false;
+                var that = this;
+                $.getJSON('mockData.json', function (data) {
+                    var listUnFollowed = [];
+                    $(data.searchList).each(function () {
+                        this.hasFollowed == false && listUnFollowed.push(this);
+                    });
+
+                    that.stat.searchList = listUnFollowed;
+
                 });
                 return false;
-                console.log(this.stat);
-                var that=this;
-                var keywords=this.stat.searchVal;
+
+                var keywords = this.stat.searchVal;
                 var searchType = 1; //[0-exact 1-fuzzy]
                 RongIMClient.getInstance().searchPublicService(searchType, keywords, {
-                    onSuccess:function(list){
+                    onSuccess: function (list) {
                         console.log("查找公众号 成功");
-                        if(list.length === 0){
-                            that.stat.searchResult= false;
-                        }else {
-                            that.stat.searchResult= true;
-                            that.stat.searchList=list;
+                        if (list.length === 0) {
+                            that.stat.searchResult = false;
+                        } else {
+                            that.stat.searchResult = true;
+                            var listUnFollowed = $(list).filter(function (index) {
+                                return $('strong', this).length == 1;
+                            });
+                            that.stat.searchList = list;
                         }
 
                     },
-                    onError:function(error){
+                    onError: function (error) {
                         console.log("查找公众号 失败");
                     }
                 });
             },
-            goPublicInfo:function (item) {
+            goPublicInfo: function (item) {
                 this.stat.currentView = 'publicInfo';
                 this.stat.currentPublic = item;
             },
-            goPublicList:function(){
-                this.stat.currentView="publicList";
+            goPublicList: function () {
+                this.stat.currentView = "publicList";
             }
         },
-        mounted:function () {
+        mounted: function () {
 
         }
     };
