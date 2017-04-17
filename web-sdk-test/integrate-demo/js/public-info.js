615d3eb83e534c52b59a55c19cea0a79d6938506
王成阔 <wangchengkuo@rongcloud.cn>
2017-04-17 11:33:28 +0800

    fix:修改引用imlib库文件 更改测试账号信息 list search info页面取消模拟数据使用真实数据接口

--- web-sdk-test/integrate-demo/js/public-info.js
+++ web-sdk-test/integrate-demo/js/public-info.js
@@ -10,25 +10,34 @@ function getPublicInfo(_options) {
                 this.stat.currentView = 'publicChat';
             },
             unsubscribePublic: function () {
+                var that=this;
                 var publicServiceType = RongIMLib.ConversationType.APP_PUBLIC_SERVICE; //固定值
                 var publicServiceId = this.stat.currentPublic.publicServiceId;
                 RongIMClient.getInstance().unsubscribePublicService(publicServiceType, publicServiceId, {
                     onSuccess: function (list) {
                         console.log("取消订阅公众号 成功");
                         console.log(list);
+                        that.stat.currentPublic.hasFollowed=false;
                     },
                     onError: function (error) {
                         console.log("取消订阅公众号 失败");
+                        if ( error ==39001){
+                            console.log("默认订阅公众号 不能被取消");
+                        }
+
+
                     }
                 });
             },
             subscribePublic: function () {
+                var that=this;
                 var publicServiceType = RongIMLib.ConversationType.APP_PUBLIC_SERVICE; //固定值
                 var publicServiceId = this.stat.currentPublic.publicServiceId;
-                RongIMClient.getInstance().unsubscribePublicService(publicServiceType, publicServiceId, {
+                RongIMClient.getInstance().subscribePublicService(publicServiceType, publicServiceId, {
                     onSuccess: function (list) {
                         console.log("订阅公众号 成功");
                         console.log(list);
+                        that.stat.currentPublic.hasFollowed=true;
                     },
                     onError: function (error) {
                         console.log("订阅公众号 失败");
