956eab6b0e88deda1a4d4ecde54cb5c43bf949e1
王成阔 <wangchengkuo@rongcloud.cn>
2017-04-25 18:27:36 +0800

    fix: init 方法整理

--- web-sdk-test/integrate-demo/conversation-list/conversation-list.js
+++ web-sdk-test/integrate-demo/conversation-list/conversation-list.js
@@ -1,28 +1,31 @@
-function getConversationList(infos) {
-
-    //初始化 vue
-    var conversationListVue = initVue();
-    //console.log(conversationListVue);
-    conversationListVue.stat.currentUserInfo = infos["userInfos"]["user1"];
+function getConversationList(app) {
 
     //同步会话列表
     RongIMClient.getInstance().getConversationList({
         onSuccess: function (conversation) {
             //console.log(conversation);
             if (conversation.length) {
-                conversation.forEach(function (item) {
-                    if (item.conversationType === 1) {
-                        //私聊
-                        item["userInfo"] = infos["userInfos"][item.latestMessage.senderUserId]
+                var infos;
+                $.getJSON('conversation-list.json',function (data) {
+                    infos=data;
+                }).then(function () {
+                    app.stat.currentUserInfo = infos["userInfos"]["user1"];
+                }).then(function () {
+                    conversation.forEach(function (item) {
+                        if (item.conversationType === 1) {
+                            //私聊
+                            item["userInfo"] = infos["userInfos"][item.latestMessage.senderUserId]
 
-                    } else if (item.conversationType === 3) {
-                        //群组
-                        item["userInfo"] = infos["groupInfos"][item.latestMessage.targetId]
-                    }
+                        } else if (item.conversationType === 3) {
+                            //群组
+                            item["userInfo"] = infos["groupInfos"][item.latestMessage.targetId]
+                        }
 
+                    });
+                    //console.log(conversation);
+                    app.stat.conversationList = conversation;
                 });
-                //console.log(conversation);
-                conversationListVue.stat.conversationList = conversation;
+
             }
         },
         onError: function (error) {
