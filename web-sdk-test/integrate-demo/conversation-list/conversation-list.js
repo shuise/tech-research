c863b8f9f10ece6bfb45ed67f2a5d838f381d874
王成阔 <wangchengkuo@rongcloud.cn>
2017-04-26 15:18:08 +0800

    fix: removeConversarion

--- web-sdk-test/integrate-demo/conversation-list/conversation-list.js
+++ web-sdk-test/integrate-demo/conversation-list/conversation-list.js
@@ -1,46 +1,53 @@
-function getMockData(url,params,callback) {
+function getMockData(url, params, callback) {
     //url 处理 '/user/get_user'
-    //params userId
-    var opt;
-    if(url === '/user/get_user'){
-        opt = 'userInfos'
-    }else if(url === '/user/get_group'){
-        opt = 'groupInfos'
-    }
-    $.getJSON('conversation-list.json',function (data) {
-        callback(data[opt][params])
+
+    $.getJSON('conversation-list.json', function (data) {
+
+
+        var userInfos = {};
+
+        params.forEach(function (item) {
+            if (item.conversationType === 1) {
+                userInfos[item.id] = data['userInfos'][item.id];
+            } else if (item.conversationType === 3) {
+                userInfos[item.id] = data['groupInfos'][item.id];
+            }
+        });
+
+        callback(userInfos)
     });
 }
 
 
-function transConversations(conversation,callback) {
+function transConversations(conversation, callback) {
 
     if (conversation.length) {
 
+        //获取需要查询用户列表
+        var conversationUsers = [], userInfos;
         conversation.forEach(function (item) {
-            if (item.conversationType === 1) {
-                //私聊
-                var userId=item.latestMessage.senderUserId;
-                getMockData("/user/get_user",userId,function (data) {
-                    item["userInfo"] = data;
-                })
-
-            } else if (item.conversationType === 3) {
-                //群组
-                var targetId=item.latestMessage.targetId;
-                getMockData("/user/get_group",targetId,function (data) {
-                    item["userInfo"] = data;
-                })
-            }
-
+            conversationUsers.push({conversationType: item.conversationType, id: item.latestMessage.targetId});
         });
 
-        callback(conversation);
+        //获取用户信息
+        getMockData("/user/get_user", conversationUsers, function (data) {
+            userInfos = data;
+
+            conversation.forEach(function (item) {
+
+                item["userInfo"] = userInfos[item.latestMessage.targetId];
+
+            });
+
+            callback(conversation);
+        });
+
+
     }
 
 }
 
-function renderConversationView(conversation) {
+function renderConversationView(conversation,instance) {
 
     return new Vue({
         el: '#conversationListPage',
@@ -61,36 +68,54 @@ function renderConversationView(conversation) {
             conversationList: {
                 props: ['stat'],
                 template: '#conversationList',
-                methods:{
-                    removeConversation:function (conversationType,targetId,index) {
-                        var that=this;
-                        RongIMClient.getInstance().removeConversation(conversationType,targetId,{
-                            onSuccess:function(bool){
+                methods: {
+                    /*removeConversation: function (conversationType, targetId, index) {
+                        var that = this;
+                        RongIMClient.getInstance().removeConversation(conversationType, targetId, {
+                            onSuccess: function (bool) {
                                 //删除会话成功。
                                 console.log(bool);
                                 //删除本地数据
-                                that.stat.conversationList.splice(index,1);
+                                that.stat.conversationList.splice(index, 1);
                             },
-                            onError:function(error){
+                            onError: function (error) {
                                 // error => 删除会话的错误码
                                 console.log(error);
                             }
                         });
+                    },*/
+                    removeThisConversation:function (conversationType, targetId, index ) {
+                        var that = this;
+                        removeConversation(instance, conversationType, targetId, index, function () {
+                            that.stat.conversationList.splice(index, 1);
+                        })
                     },
-                    clearConversation:function () {
-                        var conversationTypes = [RongIMLib.ConversationType.PRIVATE,RongIMLib.ConversationType.GROUP];
-                        RongIMClient.getInstance().clearConversations(conversationTypes,{
-                            onSuccess:function(bool){
+                    clearConversation: function () {
+                        var conversationTypes = [RongIMLib.ConversationType.PRIVATE, RongIMLib.ConversationType.GROUP];
+                        RongIMClient.getInstance().clearConversations(conversationTypes, {
+                            onSuccess: function (bool) {
                                 // 清除会话成功
                                 console.log(bool);
                             },
-                            onError:function(error){
+                            onError: function (error) {
                                 // error => 清除会话错误码。
                             }
                         });
                     }
                 }
             }
+        }
+    });
+}
+
+function removeConversation(instance, conversationType, targetId, index,callback) {
+    instance.removeConversation(conversationType, targetId, {
+        onSuccess: function () {
+            callback();
+        },
+        onError: function (error) {
+            // error => 删除会话的错误码
+            console.log(error);
         }
     });
 }
