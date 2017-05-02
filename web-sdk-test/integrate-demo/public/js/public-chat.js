772067013f51b310dafdbcf748ef78d47d113772
王成阔 <wangchengkuo@rongcloud.cn>
2017-05-02 16:50:47 +0800

    fix: 公众号部分取消单独 init.js

--- web-sdk-test/integrate-demo/public/js/public-chat.js
+++ web-sdk-test/integrate-demo/public/js/public-chat.js
@@ -1,7 +1,7 @@
 /**
  * Created by wangchengkuo on 17/4/12.
  */
-function getPublicChat(_options) {
+function getPublicChat(instance,_options) {
     var options = {
         props: ['stat'],
         template: 'template/public-chat.html',
@@ -22,7 +22,7 @@ function getPublicChat(_options) {
                 var msg = new RongIMLib.TextMessage({content: text, extra: "公众号"});
                 var conversationtype = RongIMLib.ConversationType.PUBLIC_SERVICE;
                 var targetId = this.stat.currentPublic.publicServiceId;
-                RongIMClient.getInstance().sendMessage(conversationtype, targetId, msg, {
+                instance.sendMessage(conversationtype, targetId, msg, {
                         // 发送消息成功
                         onSuccess: function (message) {
                             //message 为发送的消息对象并且包含服务器返回的消息唯一Id和发送消息时间戳
@@ -63,7 +63,7 @@ function getPublicChat(_options) {
             },
             renderHistoryMessages: function (targetId, callback) {
                 //获取历史消息
-                RongIMClient.getInstance().getHistoryMessages(RongIMLib.ConversationType.PUBLIC_SERVICE, targetId, 0, 20, {
+                instance.getHistoryMessages(RongIMLib.ConversationType.PUBLIC_SERVICE, targetId, 0, 20, {
                     onSuccess: function (list, hasMsg) {
                         //console.log(arguments);
                         callback(list, hasMsg);
