258f6eb9465655dd7691d2156200766e9d11d73f
薛争锋 <xuezhengfeng@rongcloud.cn>
2017-10-19 09:37:18 +0800

    sync: remove xiaochengxu

--- xiaochengxu/pages/config.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-  uploadURL: 'http://upload.qiniu.com',
-  getBase64URL: 'http://127.0.0.1:9090/base64'
-};
--- xiaochengxu/utils/ajax.php
+++
@@ -1,7 +0,0 @@
-<?php
-    $callback = $_GET['callback'];
-    $a = $_GET['a'];
-    // $b = $_POST['data'];
-    $b = file_get_contents('php://input');
-    echo $callback.'("'.$a.'-'.$b.'")';
-?>
