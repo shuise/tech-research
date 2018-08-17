e1629d9ebece0425317521d7ebfe3a65cf2dd04b
薛争锋 <xuezhengfeng@rongcloud.cn>
2018-08-17 16:00:27 +0800

    feat: params adjust

--- colors/opposite.js
+++ colors/opposite.js
@@ -7,15 +7,17 @@ function getOppositeColor(c1){
 
 	// Delta = Math.sqrt( Math.pow(L1 - L2) + Math.pow(s1 - s2) + Math.pow(h1 - h2));
 
-	var arr1 = c1.split("(")[1].split(")")[0].split(" ").join("").split(","); 
+	c1 = c1.split("(")[1].split(")")[0].split(" ").join("").split(","); 
 
-	var h2 = 360 - arr1[0];
-	var L2 = 100 - arr1[1].split("%")[0]/1;
-	var s2 = 100 - arr1[2].split("%")[0]/1;
+	var h2 = 360 - c1[0];
+	var L2 = 100 - c1[1].split("%")[0]/1;
+	var s2 = 100 - c1[2].split("%")[0]/1;
 
-	var arr2 = [h2, L2 + "%"  , s2 + "%"];
+	// var arr2 = [ h2, L2 + "%"  , s2 + "%"];
 
-	var c2 = "hsl(" + arr2.join(",") + ")";
+	var c2 = [ Math.ceil(h2*0.68), Math.floor(L2/1.6) + "%"  , Math.ceil(s2/1.6) + "%"];
+
+	c2 = "HSL(" + c2.join(",") + ")";
 
 	return c2;
 }
